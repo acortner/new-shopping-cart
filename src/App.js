538305a341cfa307872f9e42a438627788862cac
Andrew Cortner <andrewcortner2021@u.northwestern.edu>
2020-01-16 20:51:05 -0600

    static catalog display

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import "rbx/index.css";
+import { Button, Container, Message, Title, Card } from "rbx";
 
 const App = () => {
   const [data, setData] = useState({});
@@ -13,9 +15,18 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+    <div class="card_container">
+      {products.map(product => <Card key={product.sku}><img src={"./data/products/" + product.sku + "_1.jpg"}/>
+        <h3 id="title">{product.title}</h3>
+        <p id="description">{product.description}</p>
+        <h2 id="price">{"$" + product.price.toFixed(2)}</h2>
+        <div class="sizes">
+          <Button>S</Button>
+          <Button>M</Button>
+          <Button>L</Button>
+          <Button>XL</Button>
+        </div></Card>)}
+    </div>
   );
 };
 
